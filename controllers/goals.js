e5826fa810e3fca5ca3f9c5ad1e86a8185f504d0
bookofrage921 <bookert921@gmail.com>
2020-11-17 12:57:27 -0800

    establishes backend connection to goals

---
+++ controllers/goals.js
@@ -0,0 +1,68 @@
+const db = require('../models')
+
+//working
+const index = (req, res) => {
+    db.goal.findAll().then((foundGoals) => {
+        if(!foundGoals) return res.json({
+            message: 'No goals found in database.'
+        })
+
+        res.status(200).json({ goals: foundGoals });
+    })
+}
+
+//not working
+const show = (req, res) => {
+    console.log('in the show route')
+    console.log(req.params)
+  //not sure React side?
+    db.goal.findByPk(req.params.id).then((foundGoal) => {
+        if (!foundGoal) return res.json({
+            message: 'goal with provided ID not found.'
+        })
+        
+        res.status(200).json({ goal: foundGoal })
+    })
+}
+
+//working
+const create = (req, res) => {
+    db.goal.create(req.body).then((savedGoal) => {
+        // Validations and error handling here
+        res.status(200).json({ goal: savedGoal })
+    })
+}
+
+//not sure need to get show page working first
+const update = (req, res) => {
+    db.goal.update({
+        ...req.body
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then((updatedgoal) => {
+        if (!updatedgoal) return res.json({
+            message: "No goal with that ID found."
+        })
+        // Validations and error handling here
+        res.status(200).json({ goal: updatedgoal })
+    })
+}
+//not sure
+const destroy = (req, res) => {
+    db.goal.destroy({
+        where: { id: req.params.id }
+    }).then(() => {
+        res.status(200)
+    })
+}
+
+
+module.exports = {
+    index,
+    show,
+    create,
+    update,
+    destroy
+}
