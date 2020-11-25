f39c8ea06ba31f56ec32bf0fe1e79fe357aa0477
Booker T <book@pop-os.localdomain>
2020-11-25 14:31:32 -0800

    changed string to integer in backend

--- migrations/20201117182246-create-goal.js
+++ migrations/20201117182246-create-goal.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       userId: {
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER
       },
       completed: {
         type: Sequelize.BOOLEAN
